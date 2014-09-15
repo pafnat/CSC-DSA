5aa33e99f55295f4fe448792344721401a4e70dd
pafnat <gothica@inbox.ru>
2014-09-16 00:57:15 +0400

    some code for module #1

---
+++ src/com/dsa/module1/fibb/PeerReview.java
@@ -0,0 +1,7 @@
+package com.dsa.module1.fibb;
+
+/**
+ * Created by pafnat on 15/09/14.
+ */
+public class PeerReview {
+}
---
+++ src/com/dsa/module1/overview/Main.java
@@ -0,0 +1,7 @@
+package com.dsa.module1.overview;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
